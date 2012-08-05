0f32a831b796b2a721c2681baf1393bd68e29e23
Brian Galok <brian.galok@gmail.com>
2012-08-05 17:10:26 -0400

    Change Check and Auto check for solve
    Changed Check function to inform user if current enters are all valid.
    Solve (modified from original check function) runs after value is
    changed, stop timer and informs user puzzle is complete and gives time.

--- javascripts/sudoku.js
+++ javascripts/sudoku.js
@@ -2,6 +2,7 @@ var LastFocus = '#A1';
 var Type ='easy';//used for puzzle type-difficulty
 var interval; //used for timer
 var puzzle; 
+var completed = false;
 
 /*function setLastFocus() {
   var ae = document.activeElement;
@@ -33,7 +34,7 @@ $("#typeMenu a").click(function(e){
 			if(value > 0 && value <10){
 			  $("#" + row + col).val(value);
 			  //console.log("Row: " + row + ", Column: " + col + ", Value: " + initValues.substr(cnt,1));
-			 // $("#" + row + col).attr('disabled','disabled');
+			 $("#" + row + col).attr('disabled','disabled');
 			}
 			cnt++;
 		}
@@ -82,24 +83,27 @@ function changeValue(value){
 	noteIndex = value-1;
 	//alert("set cell to: " + value);
 	$ae = $(document.activeElement);
-
-	if(!$('#Note').hasClass('notes')){	
-		//console.log("if");
-		$ae.val(value);
-	}else if(value != ''){
-		//console.log("if else note before" + $notes[noteIndex]);
-		$notes = $ae.attr("placeholder");
-		if($notes[noteIndex] == " "){
-			var strNote = $notes.slice(0,noteIndex) + value + $notes.slice(value,9);
-		} else {
-			var strNote = $notes.slice(0,noteIndex) + " " + $notes.slice(value,9);
-		}	
-		//console.log("if else note after" + $notes[value-1]);
-		$ae.attr("placeholder", strNote);	
-	}else {
-		//console.log("else");
-		$ae.attr("placeholder","         ");
+	if(!completed) {
+		if(!$('#Note').hasClass('notes')){	
+			//console.log("if");
+			$ae.val(value);
+		}else if(value != ''){
+			//console.log("if else note before" + $notes[noteIndex]);
+			$notes = $ae.attr("placeholder");
+			if($notes[noteIndex] == " "){
+				var strNote = $notes.slice(0,noteIndex) + value + $notes.slice(value,9);
+			} else {
+				var strNote = $notes.slice(0,noteIndex) + " " + $notes.slice(value,9);
+			}	
+			//console.log("if else note after" + $notes[value-1]);
+			$ae.attr("placeholder", strNote);	
+		}else {
+			//console.log("else");
+			$ae.attr("placeholder","         ");
+		}
+		solve();
 	}
+	
 } 
 
 function focus() {
@@ -116,7 +120,9 @@ function getBoardState(){
 function resetBoard(){
 	$("input:enabled").val("");
 }
+
 function check(){
+<<<<<<< HEAD
 	// need to fix do that solution is array; not string 
 	var temp = puzzle.solution;
 	var solution = temp.split("");
@@ -125,6 +131,66 @@ function check(){
 	//$("input:enabled").each(function(){
   //	current_vals.push($(this).val());
   //});
+=======
+	if(!completed) {
+		// need to fix this function to work with JSON!!!
+		var solution = puzzle.solution;
+		//var solution = ["9", "1", "2", "7", "6", "5", "1", "9", "6", "2", "8", "4", "3", "1", "5", "7", "6", "9", "7", "1", "8", "9", "4", "6", "1", "1", "2", "6", "9"];
+		var current_vals = '';
+	
+		for(var row = 0; row <= 8; row++){
+			for(var col=0; col <=8 ; col++){
+				var crntVal = $("#" + row + col).val();
+				if(crntVal)
+					current_vals +=  $("#" + row + col).val();
+				else
+					current_vals += " ";
+			}
+		}
+		console.log(current_vals);
+		
+		var status=true;
+		var correct = wrong = blank =0;
+		for(var i=0; i<solution.length; i++){
+			if(solution[i]==current_vals[i]){
+				correct++;	
+			}
+			else if(!current_vals[i] || current_vals[i] == ' '){
+				blank++;
+			}else{
+			//	console.log('value: ' + current_vals[i]);
+				status=false;
+				wrong++;
+			}
+		}
+		if(status){
+		
+		//change to navigator for phonegap app
+			alert("Only " + blank + " cells left to finish!");
+		//	navigator.notification.alert("Only " + blank + " cells left to finish!", ,"Puzzle is Valid.","OK");
+		}
+		else {
+		//change to navigator for phonegap app
+			alert("Incorrect: " + wrong + ", Blank: " + blank);
+		//	navigator.notification.alert("Incorrect: " + wrong + ", Blank: " + blank, ,"Incorrect!!","Keep Trying!");
+		}
+	}
+}
+
+function solve(){
+	// need to fix this function to work with JSON!!!
+	var solution = puzzle.solution;
+	//var solution = ["9", "1", "2", "7", "6", "5", "1", "9", "6", "2", "8", "4", "3", "1", "5", "7", "6", "9", "7", "1", "8", "9", "4", "6", "1", "1", "2", "6", "9"];
+	var current_vals = '';
+
+  
+	for(var row = 0; row <= 8; row++){
+		for(var col=0; col <=8 ; col++){
+			current_vals +=  $("#" + row + col).val();
+		}
+	}
+ 
+>>>>>>> Change Check and Auto check for solve
 	var status=true;
 	//for(i=0; i<solution.length; i++){
 	i=0;
@@ -143,16 +209,23 @@ function check(){
 		}
 	}
 	if(status){
+<<<<<<< HEAD
 		var rep = confirm("Congratulations! You solved it!\nStart a new game?");
 		if(rep){
 			// if ok is pressed then redirect to difficulty page
 			window.location.replace("index.html");
 		}
+=======
+		clearInterval(interval);
+		alert("Completed time: " + $('#Clock').text());
+		completed = true;
+>>>>>>> Change Check and Auto check for solve
 		// also need to stop timer if correct
 		clearInterval(interval);
 	}
-	else {alert("Incorrect");}
+
 }
+
 function startTimer(time){
 	//var prev_time=time;
 	var h=time.getHours(), m=time.getMinutes(), s=time.getSeconds();
@@ -261,17 +334,18 @@ function pause() {
       aMatch=re.exec(time),
       seconds=(3600*aMatch[1]|0)+(60*aMatch[2]|0)+(aMatch[3]|0);
 			milliseconds=1000*seconds;
-
-	$("#paused").toggle();	
-	//would need to stop timer when implemented
-	if($('#paused').is(":visible")){
-	//this stops timer
-	console.log("True");
-	clearInterval(interval);
-	} else {
-	//need to figure out how to resume timer
-		console.log("False");
-		restartTimer(milliseconds);
+	if(!completed) {
+		$("#paused").toggle();	
+		//would need to stop timer when implemented
+		if($('#paused').is(":visible")){
+			//this stops timer
+			console.log("True");
+			clearInterval(interval);
+		} else {
+		//need to figure out how to resume timer
+			console.log("False");
+			restartTimer(milliseconds);
+		}
 	}
 }
 
